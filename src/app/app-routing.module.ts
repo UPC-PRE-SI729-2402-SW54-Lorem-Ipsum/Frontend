126750137db8ffaffe5809858317773a65a976d3
CamilaLoli <u202110385@upc.edu.pe>
2024-11-15 02:26:37 -0500

    feat(iam): added the page for add specialization and price for lawyer.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,9 @@ import {SignUpLawyerComponent} from "./iam/pages/sign-up-lawyer/sign-up-lawyer.c
 import {SignUpClientComponent} from "./iam/pages/sign-up-client/sign-up-client.component";
 import {ForLawyersComponent} from "./iam/pages/for-lawyers/for-lawyers.component";
 import {ChooseRoleComponent} from "./iam/pages/choose-role/choose-role.component";
+import {
+  AddSpecializationAndPriceComponent
+} from "./iam/pages/add-specialization-and-price/add-specialization-and-price.component";
 
 const routes: Routes = [
   {
@@ -22,6 +25,7 @@ const routes: Routes = [
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'choose-role', component: ChooseRoleComponent },
   { path: 'for-lawyers', component: ForLawyersComponent },
+  { path: 'add-specialization-and-price', component: AddSpecializationAndPriceComponent },
   { path: 'sign-up-lawyer', component: SignUpLawyerComponent },
   { path: 'sign-up-client', component: SignUpClientComponent },
 
