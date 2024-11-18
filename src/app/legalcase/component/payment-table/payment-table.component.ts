2b8b881331764315a8dbfd8104522dca53051893
CamilaLoli <u202110385@upc.edu.pe>
2024-11-18 03:01:30 -0500

    feat(legal-case): added style and design for payment table.

--- src/app/legalcase/component/payment-table/payment-table.component.ts
+++ src/app/legalcase/component/payment-table/payment-table.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MatPaginator} from "@angular/material/paginator";
+import {Router} from "@angular/router";
+import {MatDialog} from "@angular/material/dialog";
+import {AddDocumentComponent} from "../add-document/add-document.component";
+import {AddPaymentComponent} from "../add-payment/add-payment.component";
 
 @Component({
   selector: 'app-payment-table',
@@ -6,5 +11,29 @@ import { Component } from '@angular/core';
   styleUrl: './payment-table.component.css'
 })
 export class PaymentTableComponent {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  showPopup = false;
+
+  constructor(private router: Router, private dialog: MatDialog) {
+  }
+
+  goBackToLegalCase() {
+    this.router.navigate(['/view-legal-case']);
+  }
+
+  openAddPaymentDialog(action: 'add') {
+    /*this.legalCaseService.closeLegalCase(this.legalCase.id).subscribe(() => {
+      this.router.navigate(['/notifications']);
+    });*/
+
+    const dialogRef = this.dialog.open(AddPaymentComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result === 'add') {
+        this.showPopup = true;
+      }
+    });
+
+  }
 }
